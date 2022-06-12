8fa8d305a580b0c191625eb424e96d6287c80789
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-12 17:27:30 +0300

    commit tree fed45cf0900671017261d45844c9602e96a0b3aa

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+document.body.append(newHeader);
+newHeader.innerHTML = "Tonny is the champion";
